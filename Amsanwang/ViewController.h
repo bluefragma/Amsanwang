0e6cbb7fe6b03187f6e4f7991e29b0abacf91745
JungHoon <JungHoon@JungHoonui-MacBook-Pro.local>
2014-08-16 11:47:59 +0900

    아울렛 연결 완료

--- Amsanwang/ViewController.h
+++ Amsanwang/ViewController.h
@@ -9,5 +9,14 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *firstNumberLabel;
+@property (weak, nonatomic) IBOutlet UILabel *secondNumberLabel;
+@property (weak, nonatomic) IBOutlet UILabel *operatorLabel;
+@property (weak, nonatomic) IBOutlet UITextField *answerField;
+
+
+
+- (IBAction)done:(id)sender;
+
 
 @end
