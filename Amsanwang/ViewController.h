7ebf72a5bb91321d7083af44de15ec457ca5e718
Jeongyeon Park <ririsid@gmail.com>
2014-09-13 09:28:17 +0900

    주석 추가

--- Amsanwang/ViewController.h
+++ Amsanwang/ViewController.h
@@ -10,11 +10,11 @@
 
 @interface ViewController : UIViewController
 
-@property (weak, nonatomic) IBOutlet UILabel *leftOperandLabel;
-@property (weak, nonatomic) IBOutlet UILabel *rightOperandLabel;
-@property (weak, nonatomic) IBOutlet UILabel *operatorLabel;
-@property (weak, nonatomic) IBOutlet UITextField *answerField;
+@property (weak, nonatomic) IBOutlet UILabel *leftOperandLabel;     // 좌측 피연산자 라벨
+@property (weak, nonatomic) IBOutlet UILabel *rightOperandLabel;    // 우측 피연산자 라벨
+@property (weak, nonatomic) IBOutlet UILabel *operatorLabel;        // 연산자 라벨
+@property (weak, nonatomic) IBOutlet UITextField *answerField;      // 응답 필드
 
-- (IBAction)done:(id)sender;
+- (IBAction)done:(id)sender;    // 확인 버튼
 
 @end
