316edc286a31a4b4cdf7c72b3e519265d476010c
Jeongyeon Park <ririsid@gmail.com>
2014-09-20 20:30:12 +0900

    지역 변수 사용.

--- Amsanwang/ViewController.m
+++ Amsanwang/ViewController.m
@@ -53,26 +53,33 @@ static NSString * const kASWOperatorStringDivision = @"÷";
     // 피연산자 선택 범위.
     // 첫 번째 인자는 시작 값, 두 번째 인자는 길이.
     // NSMakeRange(0, 9)는 0부터 8까지를 나타낸다.
-    self.operandRange = NSMakeRange(1, 9);
+    NSRange operandRange = NSMakeRange(1, 9);
+    self.operandRange = operandRange;
     
     // 난수를 생성한다.
-    self.leftOperandValue = [self integerRandomValueWithRange:self.operandRange];
-    self.rightOperandValue = [self integerRandomValueWithRange:self.operandRange];
-    
-    // 좌우 라벨에 생성된 난수를 할당한다.
-    // NSInteger 값을 NSNumber 리터럴 표현식으로 바꾸고, 다시 문자열 값으로 변경한다.
-    self.leftOperandLabel.text = [@(self.leftOperandValue) stringValue];
-    self.rightOperandLabel.text = [@(self.rightOperandValue) stringValue];
+    NSInteger leftOperandValue = [self integerRandomValueWithRange:operandRange];
+    NSInteger rightOperandValue = [self integerRandomValueWithRange:operandRange];
+    self.leftOperandValue = leftOperandValue;
+    self.rightOperandValue = rightOperandValue;
     
     // 연산자 선택 범위.
-    self.operatorRange = NSMakeRange(0, 3);
+    NSRange operatorRange = NSMakeRange(0, 3);
+    self.operatorRange = operatorRange;
     
     // 연산자 랜덤 선택.
-    self.currentOperator = [self integerRandomValueWithRange:self.operatorRange];
-    self.operatorLabel.text = [self stringOperator:self.currentOperator];
+    ASWOperator currentOperator = [self integerRandomValueWithRange:operatorRange];;
+    self.currentOperator = currentOperator;
     
     // 정답을 생성한다.
-    self.answer = [self calculateWithNumber:self.leftOperandValue otherNumber:self.rightOperandValue operator:self.currentOperator];
+    self.answer = [self calculateWithNumber:leftOperandValue otherNumber:rightOperandValue operator:currentOperator];
+    
+    // 좌우 라벨에 생성된 난수를 표시한다.
+    // NSInteger 값을 NSNumber 리터럴 표현식으로 바꾸고, 다시 문자열 값으로 변경한다.
+    self.leftOperandLabel.text = [@(leftOperandValue) stringValue];
+    self.rightOperandLabel.text = [@(rightOperandValue) stringValue];
+    
+    // 연산자 라벨에 연산자 기호를 표시한다.
+    self.operatorLabel.text = [self stringOperator:currentOperator];
 }
 
 - (void)didReceiveMemoryWarning
@@ -86,7 +93,7 @@ static NSString * const kASWOperatorStringDivision = @"÷";
 - (IBAction)done:(id)sender
 {
     NSInteger answer = self.answer;
-    if (answer == [self.answerField.text intValue]) {
+    if (answer == [self.answerField.text integerValue]) {
         UIAlertView *alerview = [[UIAlertView alloc] initWithTitle:nil message:@"정답입니다" delegate:nil cancelButtonTitle:@"확인" otherButtonTitles:nil];
         [alerview show];
     }
