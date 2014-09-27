071d4c3e2dd4355c9e516ff453b4afaa457ebe13
Jeongyeon Park <ririsid@gmail.com>
2014-09-27 11:15:53 +0900

    시작 화면 추가. 내비게이션 컨트롤러 추가. 시작 화면 -> 메인 화면 모달 세그 추가.

---
+++ Amsanwang/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  Amsanwang
+//
+//  Created by Jeongyeon Park on 9/27/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
